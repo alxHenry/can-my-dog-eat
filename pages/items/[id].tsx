f431615d7c8004d6df1cc08584f0f03ce2b687f6
Alex H <5092131+alxHenry@users.noreply.github.com>
2020-12-19 22:09:33 -0800

    Display info from our db

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -2,24 +2,50 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { FC } from "react";
 import { connectToDatabase } from "../../util/mongodb";
+import { ObjectId } from "mongodb";
 
-const Item: FC = () => {
-  const router = useRouter();
-  const { id } = router.query;
+interface ItemProps {
+  id: string;
+  name: string;
+  description: string;
+}
 
-  return <p>Item: {id}</p>;
+const Item: FC<ItemProps> = ({ id, name, description }) => {
+  return (
+    <>
+      <p>ID: {id}</p>
+      <p>Name: {name}</p>
+      <p>description: {description}</p>
+    </>
+  );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { db } = await connectToDatabase();
   const items = await db.collection("items").find({}).toArray();
+  const paths = items.map((item: any) => `/items/${item._id.toString()}`);
 
-  const paths = items.map((item: any) => `/items/${item._id}`);
-  return { paths, fallback: false };
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  return { props: {} };
+  if (!params?.id) {
+    return { notFound: true };
+  }
+
+  const { db } = await connectToDatabase();
+  const item = await db.collection("items").findOne({ _id: new ObjectId(params.id as string) });
+
+  return {
+    props: {
+      id: item._id.toString(),
+      name: item.name,
+      description: item.description,
+    },
+  };
 };
 
 export default Item;
