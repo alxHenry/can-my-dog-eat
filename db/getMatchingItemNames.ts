40ca4f4b321ef5de9d00a7c2c829267ac6d19fa4
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2021-02-13 17:48:38 -0800

    Fuzzy search for api endpoint

--- db/getMatchingItemNames.ts
+++ db/getMatchingItemNames.ts
@@ -5,7 +5,23 @@ export const getMatchingItemNames = async (queryText: string) => {
 
   return db
     .collection("items")
-    .find({ $text: { $search: queryText } })
-    .limit(5)
+    .aggregate([
+      {
+        $search: {
+          index: "name search",
+          autocomplete: {
+            query: "broc",
+            path: "name",
+            fuzzy: {
+              maxEdits: 2,
+              prefixLength: 2,
+            },
+          },
+        },
+      },
+      {
+        $limit: 5,
+      },
+    ])
     .toArray();
 };
